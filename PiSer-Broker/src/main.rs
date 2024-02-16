22b2e4bd84f3e7699e60831d4de468bdc3285da7
Russe420 <joauswvonr@web.de>
2024-02-16 11:47:57 +0100

    Broker ist fertig XD, alle kopiert lol Eigene funktionalitäten usw. muss ich dann halt noch rein bringen:  - topic  - und vorallem der Client

--- PiSer-Broker/src/main.rs
+++ PiSer-Broker/src/main.rs
@@ -1,78 +1,35 @@
-//https://medium.com/@yuriy.voshch/building-an-mqtt-broker-from-scratch-with-rust-and-lunatic-part-1-bff5a2a58f61
+use lunatic::{net::TcpListener, spawn_link, AbstractProcess};
+use message_queue::MessageQueue;
+use worker::worker_process;
 
-use std::io::{BufWriter, Read, stdout, Write};
-use std::time::Duration;
-use lunatic::net::TcpListener;
-use ferris_says::say;
-use lunatic::spawn_link;
+use crate::client::Client;
 
-//Up-, und Downframe für MQTT (fix header, variable payload)
-/*
-let mqtt_connect_msg = vec![
-    0x10,       // Packet Type: CONNECT
-    0x1F,       // Remaining Length: 31
-    0x00, 0x04, // Protocol Name Length: 4
-    0x4D, 0x51, 0x54, 0x54, // Protocol Name: MQTT
-    0x05,             // Protocol Level: MQTT 3.1.1
-    0xC2, 0x00,       // Connect Flags: Clean Session, Will QoS 2, Will Retain, No User Name or Password, Keep Alive: 30 seconds
-    0x00, 0x1E,       // Keep Alive: 30 seconds
-    0x00, 0x06,       // Client ID Length: 6
-    0x64, 0x65, 0x76, 0x69, 0x63, 0x65, // Client ID: device1
-    0x00, 0x08,       // Will Topic Length: 8
-    0x68, 0x6F, 0x6D, 0x65, 0x2F, 0x74, 0x65, 0x73, // Will Topic: home/test
-    0x00, 0x0A,       // Will Message Length: 10
-    0x64, 0x65, 0x61, 0x64, 0x62, 0x65, 0x65, 0x66, 0x67, 0x68, // Will Message: deadbeefgh
-];
+mod client;
+mod message_queue;
+mod worker;
 
+fn main() -> std::io::Result<()> {
+    // first, we need to start the MessageQueue
+    let message_queue = MessageQueue::start_as(&MessageQueue::default(), ())
+        .expect("should have started MessageQueue");
 
-const MQTT_CONN_ACK_MSG:Vec<u8>=vec![
-    0x20,
-    0x02,
-    0x00,
-    0x00
-];
-*/
-fn main()-> std::io::Result<()> {
-    {//Greetings
-        let stdout = stdout();
-        let writer = BufWriter::new(stdout.lock());
-        say("MQTT-Broker", 12, writer).unwrap();
-    }
+    // we're starting the worker process with spawn_link because we want everything to crash if a worker goes down
+    // as it indicates an issue with our code. This is appropriate in such an early version of our MQTT broker
+    // but at a later point we will need some supervision and possibly a dynamic pool of workers
+    let _worker = spawn_link!(|message_queue| worker_process(message_queue));
 
     let listener = TcpListener::bind("127.0.0.1:1883")?;
-    while let Ok((stream,_))=listener.accept(){
-        println!("New Client Connected:{:?}",stream.peer_addr());
-        spawn_link!(|stream| connection_handler(stream));
+    while let Ok((stream, _)) = listener.accept() {
+        println!("New client connected: {:?}", stream.peer_addr());
+        // spawn a new process for every new connection
+        spawn_link!(|stream| {
+            // if the creation of a new client instance fails the other clients
+            // will still keep running.
+            // For now we won't use this struct, but later it
+            // might come in handy for monitoring
+            Client::start_new_client(stream);
+        });
     }
-    Ok(())
-}
 
-fn connection_handler(mut stream: lunatic::net::TcpStream){
-    let mut buffer =[0;1024];
-    loop{
-        match stream.read(&mut buffer) {
-            Ok(n) if n > 0 => {
-                let message_type = buffer[0] >> 4; //Typ der Nachricht????
-                let message_length = buffer[1];//nachrichtenlänge???
-                println!("Received message of type {message_type}, with length {message_length}");
-                match message_type {
-                    1 => { // Typ
-                        let ack_msg: Vec<u8> = vec![
-                            0x20,
-                            0x02,
-                            0x00,
-                            0x00,
-                        ];
-                        stream.write(&ack_msg).expect("Failed to send Acknowledged");
-                        println!("Responded to CONNECT");
-                    }
-                    t => eprintln!("Unknown type of MQTT!"),
-                }
-            }
-            Ok(_) => { eprintln!("Nothing to read . . .");
-                lunatic::sleep(Duration::from_secs(1)); //Bullshiat Statt sleep bracuhe ich irgendwas asynchrones????
-            },
-            Err(e) => eprintln!("unbekannter Fehler: {e}"),
-        }
-    }
+    Ok(())
 }
