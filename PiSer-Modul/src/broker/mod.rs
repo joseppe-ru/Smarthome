b57373ca8c5c96599ce54e879a33c89aecbf6a75
Russe420 <joauswvonr@web.de>
2024-03-04 16:28:03 +0100

    broker expansion  Broker muss um Websocketverbindung ergänzt werden  1. Schritt: Implementation eines Chatservers  2. Integration in MQTT-Broker

--- PiSer-Modul/src/broker/mod.rs
+++ PiSer-Modul/src/broker/mod.rs
@@ -1,12 +1,27 @@
-use std::{sync::{Arc}};
-use tokio::net::{TcpStream ,TcpListener};
-use std::time::Duration;
-use tokio::{sync::Mutex};
-use tokio::time::sleep;
-use crate::broker::client::Client;
-use crate::broker::message_queue::MessageQueue;
-use crate::broker::client::tcp_stream_writer::TcpWriter;
-use crate::broker::client::tcp_stream_reader::TcpReader;
+use std::{
+    sync::Arc,
+    time::Duration,
+};
+
+use futures::{AsyncReadExt, StreamExt, TryStreamExt};
+use tokio::{
+    sync::Mutex,
+    time::sleep,
+    io::AsyncBufReadExt,
+    net::TcpListener,
+};
+use tokio::io::BufReader;
+
+use crate::broker::{
+    client::{
+        Client,
+        tcp_stream_writer::TcpWriter,
+        tcp_stream_reader::TcpReader
+    },
+    message_queue::MessageQueue,
+};
+
+use tokio_tungstenite::{accept_async, WebSocketStream};
 mod client;
 mod message_queue;
 mod worker;
@@ -18,23 +33,38 @@ pub async fn broker_setup()->Result<(),&'static str>{
     //message queue starten (mehr so ne referenz auf die Messagequeue)
     let queue = Arc::new(Mutex::new(message_queue::MessageQueue::new()));
     //worker starten (Consumer der Queue)
-    let queue_clone=Arc::clone(&queue);
-    tokio::spawn(async move { worker::worker_process(queue_clone).await });
+    let queue_clone_worker=Arc::clone(&queue);
+    let queue_clone_client=Arc::clone(&queue);
+    let queue_clone_ws_client=Arc::clone(&queue);
+    tokio::spawn(async move { worker::worker_process(queue_clone_worker).await });
 
     println!("[broker] Listener wird gestartet");
 
-    let listener = TcpListener::bind("0.0.0.0:1885").await.expect("failed to bind address");
-    
-    while let Ok((stream,_)) = listener.accept().await{
-        println!("[broker  ] Neuer MQTT_Client connected: {:?}",stream.peer_addr());
-        let queue_clone=Arc::clone(&queue);
-        tokio::spawn(handle_connect(stream,queue_clone));
-    }
+    let mqtt_listener = TcpListener::bind("0.0.0.0:1885").await.expect("failed to bind address");
+    let mqtt_ws_listener = TcpListener::bind("0.0.0.0:1886").await.expect("[broker  ] failed to bind ws-address");
+    tokio::spawn(
+        async move {
+            while let Ok((stream, _)) = mqtt_listener.accept().await {
+                println!("[broker  ] Neuer MQTT_Client connected: {:?}", stream.peer_addr());
+                let queue_clone = Arc::clone(&queue_clone_client);
+                let s = tokio::spawn(handle_connect(stream, queue_clone));
+            }
+        }
+    );
 
-    Ok(pace_holder("broker").await.unwrap())
+    //tcp-listener für Websocket mit warp:
+    //let mqtt_route=warp.any().map(|ws:ws::WS::websocket| {})...
+
+    Ok(())//Ok(pace_holder("broker").await.unwrap())
 }
 
-async fn handle_connect(tokio_stream:TcpStream,queue:Arc<Mutex<MessageQueue>>)->Result<(),&'static str>{
+async fn handle_ws_connect(tokio_stream:tokio::net::TcpStream,queue:Arc<Mutex<MessageQueue>>)->Result<(),&'static str>{
+    //websocket upgrade mit warp
+    //websocket chat server erstellen und dann in worker und jobs ... von MQTT-Broker integrieren
+Ok(())
+}
+
+async fn handle_connect(tokio_stream:tokio::net::TcpStream,queue:Arc<Mutex<MessageQueue>>)->Result<(),&'static str>{
 
         //tokio TcpStream in std TcpStream umwandeln
     let std_stream=tokio_stream.into_std().expect("[broker  ] failed to convert tokio to std");
